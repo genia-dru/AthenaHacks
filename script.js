3060b5f438bd40422bf68059b50a44c4db0f08f5
Red <saraalva@usc.edu>
2022-02-19 15:31:25 -0800

    Added timer
    Added timer- no button to start it yet but it's there!!

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//comment lol
+  var minute = 25;
+  var sec = 60;
+  setInterval(function() {
+    document.getElementById("timer").innerHTML = minute + " : " + sec;
+    sec--;
+    if (sec == 00) {
+      minute --;
+      sec = 60;
+      if (minute == 0) {
+        minute = 25;
+      }
+    }
+  }, 1000);
