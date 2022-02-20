4d098977124f74a8a22f66697cc772af0f7d548f
Red <saraalva@usc.edu>
2022-02-19 19:32:07 -0800

    Add background Image

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-  var studyTime = true;
+var minute = 24;
+var sec = 60;
 
-  var minute = 24;
-  var sec = 60;
-  setInterval(function() {
-    sec--;
-    if (sec == 00) {
-      minute --;
-      sec = 59;
-      if (minute == 0) {
-        minute = 24;
+setInterval(function() {
+      sec--;
+      if (sec == 00) {
+        minute --;
+        sec = 59;
+        if (minute == 0) {
+          minute = 24;
+        }
       }
-    }
-    if (sec < 10){
-      document.getElementById("timer").innerHTML = minute + " : 0" + sec;
-    }
-    else{
-      document.getElementById("timer").innerHTML = minute + " : " + sec;
-    }
-  }, 1000);
+      if (sec < 10){
+        document.getElementById("timer").innerHTML = minute + " : 0" + sec;
+      }
+      else{
+        document.getElementById("timer").innerHTML = minute + " : " + sec;
+      }
+      if(sec == 0 && minute == 0){
+        studyTime = false;
+      }
+    }, 1000);
