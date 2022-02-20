c3219f258d9a8ddb356a41a009b1375548f9ed27
Red <saraalva@usc.edu>
2022-02-19 21:34:33 -0800

    added sound

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ var sec = 60;
 var switched = true;
 
 var audio = document.getElementById("myAudio");
-  
-function playSound() {
-    audio.play();
-}
+//audios
+var audio1 = new Audio('waterdrops.wav');
+var audio2 = new Audio('lightrain.mp3');
+var audio3 = new Audio('heavyrain.wav');
+var audio4 = new Audio('thunder.wav');
+var audio5 = new Audio('birdscrickets.wav');
+var audio6 = new Audio('frogs.wav');
 
 function timer(){
 setInterval(function() {
@@ -39,3 +42,13 @@ setInterval(function() {
   }
   //run
   timer();
+
+  function play(a){
+  a.play()
+  }
+document.getElementById("sound1").addEventListener("click", function(){play(audio1)}, false);
+document.getElementById("sound2").addEventListener("click", function(){play(audio2)}, false);
+document.getElementById("sound3").addEventListener("click", function(){play(audio3)}, false);
+document.getElementById("sound4").addEventListener("click", function(){play(audio4)}, false);
+document.getElementById("sound5").addEventListener("click", function(){play(audio5)}, false);
+document.getElementById("sound6").addEventListener("click", function(){play(audio6)}, false);
